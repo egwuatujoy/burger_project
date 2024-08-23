e164ae8900a8f475c79fd40289bd56b517cd4343
joyegwuatu <egwuatujoy60@gmail.com>
2024-08-23 14:46:04 +0100

    updated the home section

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const navOpen = document.querySelector("#nav-open");
 const navClose = document.querySelector("#nav-close");
 const navMenu = document.querySelector("#nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+console.log(navMenu);
+
+// TOGGLE THE MENU BTN
 
 if (navOpen) {
   navOpen.addEventListener("click", () => {
@@ -13,3 +17,27 @@ if (navClose) {
     navMenu.classList.remove("show-menu");
   });
 }
+
+// REMOVE MENU MOBILE DESIGN
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", linkMenu);
+});
+
+function linkMenu() {
+  const navMenu = document.querySelector("#nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+
+// SHOW HEADER
+
+function showHeader() {
+  const headerContainer = document.querySelector("#header");
+  if (scrollY >= 50) {
+    headerContainer.classList.add("shadow-header");
+  } else {
+    headerContainer.classList.remove("shadow-header");
+  }
+}
+
+window.addEventListener("scroll", showHeader);
